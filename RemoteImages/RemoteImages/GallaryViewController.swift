ff97a082e07d41c667d417dd1990dc72d90eb688
Sagar Kadam <skadam@iaai.com>
2022-06-12 23:00:49 +0530

    Code Update : Added the UI part to show the images.

--- RemoteImages/RemoteImages/GallaryViewController.swift
+++ RemoteImages/RemoteImages/GallaryViewController.swift
@@ -17,14 +17,16 @@ class GallaryCell: UITableViewCell {
 }
 
 class GallaryViewController: UITableViewController {
-    let dataSource = ["https://assets.imgix.net/hp/snowshoe.jpg?auto=compress&w=600&h=600&fit=crop&mask=ellipse&fm=png",
-                      "https://assets.imgix.net/unsplash/transport.jpg?auto=compress&w=600&h=600&fit=crop&mask=ellipse&fm=png",
-                      "https://assets.imgix.net/unsplash/coffee.JPG?auto=compress&w=600&h=600&fit=crop&mask=ellipse&fm=png",
-                      "https://assets.imgix.net/unsplash/womansitting.jpg?auto=compress&w=600&h=600&fit=crop&mask=ellipse&fm=png",
-                      "https://assets.imgix.net/examples/balloons.jpg?auto=compress&w=600&h=600&fit=crop&mask=ellipse&fm=png",
-                      "https://assets.imgix.net/examples/mountain.jpg?auto=compress&w=600&h=600&fit=crop&mask=ellipse&fm=png",
-                      "https://assets.imgix.net/unsplash/umbrella.jpg?auto=compress&w=900&h=600&fit=crop&crop=focalpoint&fp-debug=1",
-                      "https://assets.imgix.net/examples/bluehat.jpg?auto=compress&w=900&h=600&fit=crop&crop=focalpoint&fp-debug=1"]
+
+    let dataSource = ["https://assets.imgix.net/hp/snowshoe.jpg?auto=compress&fit=fill&fm=png",
+                      "https://assets.imgix.net/unsplash/transport.jpg?auto=compress&fit=fill&fm=png",
+                      "https://assets.imgix.net/unsplash/coffee.JPG?auto=compress&fit=fill&fm=png",
+                      "https://assets.imgix.net/unsplash/womansitting.jpg?auto=compress&fit=fill&fm=png",
+                      "https://assets.imgix.net/examples/balloons.jpg?auto=compress&fit=fill&fm=png",
+                      "https://assets.imgix.net/examples/mountain.jpg?auto=compress&fit=fill&fm=png",
+                      "https://assets.imgix.net/unsplash/umbrella.jpg?auto=compress&fit=fill&fm=png",
+                      "https://assets.imgix.net/examples/bluehat.jpg?auto=compress&fit=fill&fm=png"]
+     
     
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return dataSource.count
